ed396727a75f7d33ba9e6d01eb0d2ab2ba65340f
Santiago <Santiagogbf2000@Gmail.com>
2024-07-12 17:54:24 -0400

    Added: usersRoute

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -1,6 +1,10 @@
 import express from 'express'
 import cors from 'cors'
+import usersRoutes from './routes/v1/users.routes.js'
 
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use('/api/v1/users', usersRoutes)
+
+export default app
