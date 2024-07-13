05449d3d609dcf8f65930017d42e7c6d2a55356f
Santiago <Santiagogbf2000@Gmail.com>
2024-07-13 19:56:20 -0400

    Added: Routed conditional item

--- Frontend/src/components/partials/Header.jsx
+++ Frontend/src/components/partials/Header.jsx
@@ -1,18 +1,42 @@
 import { HiMiniSignal } from 'react-icons/hi2'
 import { NavLink, Outlet } from 'react-router-dom'
+import { useSesionContext } from '../../context/SesionContext'
 export default function () {
+  const { logout } = useSesionContext()
+  const user = JSON.parse(window.localStorage.getItem('auth'))
+
   return (
     <>
       <header>
         <div>
-          <span className='isLive'>
-            Live &nbsp;&nbsp;
-            <HiMiniSignal />
-          </span>
-          <span> Lorem ipsu dolor sit amet </span>
-          <NavLink to='/login' className='to-login'>
-            <span className='isJoin'> Join Now &nbsp;&nbsp;<i className='fa-solid fa-circle-play' /></span>
+          <NavLink to='/' className='isLive'>
+            <span
+              className='isLive'
+            >
+              Live &nbsp;&nbsp;
+              <HiMiniSignal
+                className='home-icon'
+              />
+            </span>
           </NavLink>
+
+          <span> Lorem ipsu dolor sit amet </span>
+          {user
+            ? (
+              <NavLink
+                onClick={() => {
+                  logout()
+                }} to='/' className='to-login'
+              >
+                <span className='isJoin'> Cerrar Sesión &nbsp;&nbsp;<i className='fa-solid fa-circle-play' /></span>
+              </NavLink>
+              )
+            : (
+              <NavLink to='/login' className='to-login'>
+                <span className='isJoin'> Join Now &nbsp;&nbsp;<i className='fa-solid fa-circle-play' /></span>
+              </NavLink>
+              )}
+
         </div>
 
       </header>
