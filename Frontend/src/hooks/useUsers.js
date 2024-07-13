4d318bcc11f6bf95379c60a9706b2f71da12dd79
Santiago <Santiagogbf2000@Gmail.com>
2024-07-13 18:12:33 -0400

    Added: conditional render

--- Frontend/src/hooks/useUsers.js
+++ Frontend/src/hooks/useUsers.js
@@ -1,13 +1,58 @@
-import { getUsers } from '../service/user'
+import { getUsers, deleteUser, getUser } from '../service/user'
 import { useState, useEffect } from 'react'
 
 export const useUsers = () => {
   const [users, setUsers] = useState([])
+  const [user, setUser] = useState()
+  const [onOpenView, setOnOpenView] = useState(false)
+  const [onOpenEdit, setOnOpenEdit] = useState(false)
+
+  const handleDelete = (e) => {
+    const id = e.target.id.split(' ')[1]
+    console.log(id)
+    deleteUser(id).then((data) => {
+      console.log(data)
+    })
+  }
+
+  const handleEdit = (e) => {
+    const id = e.target.id.split(' ')[1]
+    getUser(id).then((data) => {
+      console.log(data)
+      setUser(data)
+    })
+  }
+
+  useEffect(() => {
+    if (user) {
+      setOnOpenEdit(true)
+    }
+  }, [user])
+
+  const handleView = (e) => {
+    // Abrimos la modal
+
+    const id = e.target.id.split(' ')[1]
+    console.log(id)
+    getUser(id).then((data) => {
+      console.log(data)
+      setUser(data)
+    })
+    setOnOpenView(true)
+  }
+
+  const handleCloseView = () => {
+    setOnOpenView(false)
+  }
+
+  const handleCloseEdit = () => {
+    setOnOpenEdit(false)
+  }
 
   useEffect(() => {
     getUsers()
       .then((data) => setUsers(data))
   }, [])
 
-  return { users }
+  return { users, handleDelete, handleEdit, handleView, onOpenEdit, onOpenView, handleCloseEdit, handleCloseView, user }
 }
