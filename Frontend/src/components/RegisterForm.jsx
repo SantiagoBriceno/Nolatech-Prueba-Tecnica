b04f337f6aedad87b83be43dae4ad044e1124786
Santiago <Santiagogbf2000@Gmail.com>
2024-07-14 17:32:20 -0400

    Fixed: Validation message

--- Frontend/src/components/RegisterForm.jsx
+++ Frontend/src/components/RegisterForm.jsx
@@ -2,7 +2,7 @@ import MyInput from './Form/MyInput'
 import './Form/MyForm.css'
 import { useRegisterUser } from '../hooks/useRegisterUser'
 export default function () {
-  const { username, password, validationUsername, validationPassword, confirmPassword, confirmUsername, handleSubmit, handleChange } = useRegisterUser()
+  const { validationUsername, validationPassword, validationName, handleSubmit, handleChange } = useRegisterUser()
   return (
     <div className='container'>
 
@@ -13,19 +13,21 @@ export default function () {
         <hr />
         <form className='register-form'>
           <div className='input-container'>
-            <p>Ingresa tu nombre</p>
+            <p className='form-label'>Ingresa tu nombre</p>
             <MyInput
-              id='nombre' placeholder='Jacinto' onChange={handleChange}
+              id='nombre' placeholder='Jacinto' validation={validationName} onChange={handleChange}
             />
           </div>
           <div className='input-container'>
-            <p>Ingresa una dirección de correo válido / Username <span>(8-30 caracteres / sin espacios )</span> </p>
+            <p className='form-label'>
+              Ingresa una dirección de correo válido / Username <span>(8-30 caracteres / sin espacios )</span>
+            </p>
             <MyInput id='username' onChange={handleChange} placeholder='Example123' type='text' name='username' validation={validationUsername} />
             <MyInput id='confirmUsername' onChange={handleChange} placeholder='Confirmar Username o correo' type='text' name='confirmUsername' />
           </div>
           {/* Los campose serán username, confirmar username, contraseña, confirmar contraseña */}
           <div className='input-container'>
-            <p>Ingresa tu contraseña <span>(mínimo 8 carácteres / sin espacios en blanco)</span></p>
+            <p className='form-label'>Ingresa tu contraseña <span>(mínimo 8 carácteres / sin espacios en blanco)</span></p>
             <MyInput id='password' onChange={handleChange} placeholder='Santi123' type='password' name='password' validation={validationPassword} />
             <MyInput id='confirmPassword' onChange={handleChange} placeholder='Confirmar contraseña' type='password' name='confirmPassword' />
           </div>
